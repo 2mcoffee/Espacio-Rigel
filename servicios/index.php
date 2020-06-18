1fa0711d5c88e5b6905fdfde5eddc2ba2d92ff11
2mcoffee <luchoalfonsin@yahoo.com.ar>
2020-06-18 16:54:23 -0300

    Servicios

---
+++ servicios/index.php
@@ -0,0 +1,79 @@
+<?php
+require_once('../include/header.php');
+require_once('../include/navbar.php');
+?>
+<br>
+<div class="text-center">
+    <h1 class="main-title">#Servicios</h1>
+</div>
+<br>
+<div class="container">
+    <div>
+        Espacio rigel ofrece un amplio abanico de opciones para tu crecimiento espiritual y personal: Terapias alternativas, Sesiones, Encuentros y Formaciones Online & Presenciales.
+    <div class="p-2"></div>
+        <strong>¡Elegí según tu necesidad y del resto, nos encargamos nosotros!.</strong>
+    </div>
+    <br>
+    <div class="card-group">
+        <div class="card">
+            <img class="card-img-top" src="../img/s01.jpg" alt="Sesión de Reiki Usui">
+            <div class="card-body">
+            <h5 class="card-title">Sesión de Reiki Usui</h5>
+            <p class="card-text">
+                Ayudamos a armonizar y desbloquear todos los planos del ser humano (físico, mental, emocional y espiritual) sin necesidad de agredir tu cuerpo.
+                <div class="p-2"></div>
+                <a role="button" href="../contacto/" class="btn btn-outline-dark btn-sm">Contactar</a>
+            </p>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted">Presencial | 55 minutos</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="../img/s02.jpg" alt="Limpieza Energética">
+            <div class="card-body">
+            <h5 class="card-title">Limpieza Energética</h5>
+            <p class="card-text">
+                Gracias a la modalidad del sahumo con hierbas (salvia, romero, lavanda, copal) realizamos la limpieza energética, curación y protección de tu hogar.
+                <div class="p-2"></div>
+                <a role="button" href="../contacto/" class="btn btn-outline-dark btn-sm">Contactar</a>
+            </p>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted">Presencial | 55 minutos</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="../img/s03.jpg" alt="Sanación con cristales">
+            <div class="card-body">
+            <h5 class="card-title">Sanación con cristales</h5>
+            <p class="card-text">
+                Mediante la utilización de los cristales logramos equilibrar la energía de tus 7 chakras, para alinearlos y sanar aquellas cuestiones que se asocien a cada uno de ellos.
+                <div class="p-2"></div>
+                <a role="button" href="../contacto/" class="btn btn-outline-dark btn-sm">Contactar</a>
+            </p>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted">Presencial | 55 minutos</small>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="../img/s04.jpg" alt="Lectura Registros Akashicos">
+            <div class="card-body">
+            <h5 class="card-title">Lectura Registros Akashicos</h5>
+            <p class="card-text">
+                Conectamos con tus registros, con tus maestros, para que te ayuden en este proceso de despertar, sanando situaciones bloqueantes.
+                <div class="p-2"></div>
+                <a role="button" href="../contacto/" class="btn btn-outline-dark btn-sm">Contactar</a>
+            </p>
+            </div>
+            <div class="card-footer">
+            <small class="text-muted">Online | 55 minutos</small>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<?php
+require_once('../include/footer.php');
+?>
